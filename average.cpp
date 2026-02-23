80cdcf0b84d250633bd62ed1e3de2aade409bdca
Methas <methas_n@cmu.ac.th>
2026-02-23 16:38:42 +0700

    "average.cpp completed"

--- average.cpp
+++ average.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main(int argc , char* argv[] ){
+
+    int n = argc-1;
+    float sum , avg;
+
+    if(argc == 1){
+        cout<<"Please input numbers to find average."; 
+    }else{
+
+        for(int i =  0; i <= n ; i++){
+            sum += atoi(argv[i]);
+        }
+        avg = sum/n;
+        cout<<"---------------------------------"<<endl;
+    
+        cout<<"Average of " << n << " numbers = "<< avg <<endl;
+
+        cout<<"---------------------------------";
+
+    }
+
+}
 
