1a8b033be7ad5d7ac6dece6e6f939ab1169022e4
Methas <methas_n@cmu.ac.th>
2026-02-24 01:16:14 +0700

    "calculator completed"

--- calculator.cpp
+++ calculator.cpp
@@ -1,12 +1,101 @@
 #include <windows.h>
+#include <cstdio>
+
+HWND textfiled,Input1,Input2;
+HWND btn_ps ,btn_mn , btn_mtp , btn_dv;
+int pstn_x = 40;
+double result,Num1,Num2;
+char input1[20],input2[20],output[100];
 
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
 		
+		case WM_CREATE: 
+			CreateWindow("STATIC",
+						 "Please input two numbers",
+						  WS_VISIBLE| WS_CHILD | WS_BORDER,
+						  20,20,200,20,
+						  hwnd, NULL, NULL, NULL);
+
+			Input1  = CreateWindow("EDIT",
+						 "",
+						  WS_VISIBLE| WS_CHILD | WS_BORDER,
+						  30,50,180,20,
+						  hwnd, NULL, NULL, NULL);
+			Input2  = CreateWindow("EDIT",
+						 "",
+						  WS_VISIBLE| WS_CHILD | WS_BORDER,
+						  30,80,180,20,
+						  hwnd, NULL, NULL, NULL);
+			
+			btn_ps = CreateWindow("BUTTON",
+						 "+",
+						  WS_VISIBLE| WS_CHILD | WS_BORDER,
+						  pstn_x+30,120,20,20,
+						  hwnd, (HMENU)1, NULL, NULL);
+			btn_mn = CreateWindow("BUTTON",
+						 "-",
+						  WS_VISIBLE| WS_CHILD | WS_BORDER,
+						  pstn_x+60,120,20,20,
+						  hwnd, (HMENU)2, NULL, NULL);
+			btn_mtp = CreateWindow("BUTTON",
+						 "*",
+						  WS_VISIBLE| WS_CHILD | WS_BORDER,
+						  pstn_x+90,120,20,20,
+						  hwnd, (HMENU)3, NULL, NULL);
+
+			btn_dv = CreateWindow("BUTTON",
+						 "/",
+						  WS_VISIBLE| WS_CHILD | WS_BORDER,
+						  pstn_x+120,120,20,20,
+						  hwnd, (HMENU)4, NULL, NULL);
+
+			
+			
+			break;
+		
+		case WM_COMMAND: {
+
+				int wmId = LOWORD(wParam);
+
+				if(wmId >=1 && wmId <=4){
+				GetWindowText(Input1,input1,20);
+				GetWindowText(Input2,input2,20);
+
+				Num1 = atof(input1);
+				Num2 = atof(input2);
+
+				switch(wmId){
+					case 1:
+						result = Num1 + Num2;
+					break;
+
+					case 2:
+						result = Num1 - Num2;
+					break;
+
+					case 3:
+						result = Num1 * Num2;
+					break;
+
+					case 4:
+					if(Num2 == 0){
+						MessageBox(hwnd, "Cannot divide by zero","Error!",MB_ICONERROR|MB_OK);
+						return 0;
+					}
+					result = Num1 / Num2;
+					break;
+				}
+				
+				sprintf(output,"%f",result);
+				MessageBox(hwnd,output,"Result",MB_OK);
+			}
+			break;
+		}
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
-			PostQuitMessage(0);
+			PostQuitMessage(0); 
 			break;
 		}
 		
@@ -31,7 +120,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.hbrBackground = (HBRUSH)(COLOR_HIGHLIGHT+1); //สี นง.สดละกัน
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,11 +130,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, /* x */
-		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_CAPTION|WS_SYSMENU,
+		650, /* x */
+		250, /* y */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
